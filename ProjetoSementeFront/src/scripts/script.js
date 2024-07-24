3299c00f470c5b46034402692dd9563f7f0462e1
julia.batista <julia.pereira@e-unicred.com.br>
2024-07-24 09:10:23 -0300

    conclusão do projeto

---
+++ ProjetoSementeFront/src/scripts/script.js
@@ -0,0 +1,9 @@
+document.getElementById('forgot-password').addEventListener('click', function(e) {
+    e.preventDefault();
+    alert('Função de recuperação de senha não implementada.');
+});
+
+document.getElementById('register').addEventListener('click', function(e) {
+    e.preventDefault();
+    alert('Função de cadastro não implementada.');
+});
